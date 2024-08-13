671686f626f7347525ea413048db9c734e477862
akira-yp <yog.puripuri@gmail.com>
2024-08-13 17:11:43 +0900

    blog機能のCRUD APIを作成

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strconv"
+
+	"encoding/json"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
@@ -46,6 +49,69 @@ func connectDB() (*gorm.DB, error) {
 	return db, err
 }
 
+func errorDB(db *gorm.DB, c *gin.Context) bool {
+	if db.Error != nil {
+		log.Printf("Error todos: %v", db.Error)
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return true
+	}
+	return false
+}
+
+func listener(r *gin.Engine, db *gorm.DB) {
+	r.GET("/blog/delete", func(c *gin.Context) {
+		id, _ := c.GetQuery("id")
+		result := db.Delete(&Blog{}, id)
+		if errorDB(result, c) {
+			return
+		}
+	})
+	r.POST("/blog/update", func(c *gin.Context) {
+		id, _ := strconv.Atoi(c.PostForm("id"))
+		title := c.PostForm("title")
+		content := c.PostForm("content")
+		var blog Blog
+		result := db.Where("id = ?", id).Take(&blog)
+		if errorDB(result, c) {
+			return
+		}
+		blog.Title = title
+		blog.Content = content
+		result = db.Save(&blog)
+		if errorDB(result, c) {
+			return
+		}
+	})
+	r.POST("/blog/create", func(c *gin.Context) {
+		title := c.PostForm("title")
+		content := c.PostForm("content")
+		fmt.Println(c.Request.PostForm, title, content)
+		result := db.Create(&Blog{Title: title, Content: content})
+		if errorDB(result, c) {
+			return
+		}
+	})
+	r.GET("/blog/get", func(c *gin.Context) {
+		var blog Blog
+		id, _ := c.GetQuery("id")
+		result := db.First(&blog, id)
+		if errorDB(result, c) {
+			return
+		}
+		fmt.Println(json.NewEncoder(os.Stdout).Encode(blog))
+		c.JSON(http.StatusOK, blog)
+	})
+	r.GET("/blog/list", func(c *gin.Context) {
+		var blogs []Blog
+		result := db.Find(&blogs)
+		if errorDB(result, c) {
+			return
+		}
+		fmt.Println(json.NewEncoder(os.Stdout).Encode(blogs))
+		c.JSON(http.StatusOK, blogs)
+	})
+}
+
 func main() {
 	r := gin.Default()
 	db, err := connectDB()
@@ -56,11 +122,9 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to migrate database: %v", err)
 	}
-	r.GET("/hoge", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "fuga",
-		})
-	})
+
+	listener(r, db)
+
 	fmt.Println("Database connection and migration successful")
 	r.Run(":8080")
 }
